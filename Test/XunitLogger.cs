45812704bdbedff112c540ca57f30b8fb2b654d4
Dries <dries.verbeke@gmail.com>
2022-02-19 17:15:31 +0100

    Setup unit test skeleton (#30)
    Co-authored-by: Dries Verbeke <verbedr@cronos.be>

---
+++ Test/XunitLogger.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using Xunit.Abstractions;
+
+namespace Test;
+
+public class XunitLogger<T> : ILogger<T>, IDisposable
+{
+    private ITestOutputHelper _output;
+
+    public XunitLogger(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        _output.WriteLine(state.ToString());
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return this;
+    }
+
+    public void Dispose()
+    {
+    }
+}
