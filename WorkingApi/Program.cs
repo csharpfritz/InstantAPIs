8fc1c1701179d357b56e97803ca767f47d6910e8
Jason Bock <jason.r.bock@outlook.com>
2022-02-24 22:07:09 -0600

    #12 confirmed new changes work in API app

--- WorkingApi/Program.cs
+++ WorkingApi/Program.cs
@@ -11,13 +11,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-var sw = Stopwatch.StartNew();
+//var sw = Stopwatch.StartNew();
 
-app.MapInstantAPIs<MyContext>(options =>
-{
-	options.IncludeTable(db => db.Contacts, (ApiMethodsToGenerate.GetById | ApiMethodsToGenerate.Get));
-});
-Console.WriteLine($"Elapsed time to build InstantAPIs: {sw.Elapsed}");
+//app.MapInstantAPIs<MyContext>(options =>
+//{
+//	options.IncludeTable(db => db.Contacts, (ApiMethodsToGenerate.GetById | ApiMethodsToGenerate.Get));
+//});
+//Console.WriteLine($"Elapsed time to build InstantAPIs: {sw.Elapsed}");
+
+app.MapMyContextToAPIs();
 
 app.UseSwagger();
 app.UseSwaggerUI();
