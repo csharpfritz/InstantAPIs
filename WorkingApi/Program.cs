8edd6fd17a89b00ade59b0a16c1f9bb56ef0821c
MK dot CS <huwaireb@finalstack.com>
2022-02-25 23:31:05 +0400

    OpenAPI implementation for InstantAPIs

--- WorkingApi/Program.cs
+++ WorkingApi/Program.cs
@@ -1,13 +1,26 @@
 using Fritz.InstantAPIs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Diagnostics;
 using WorkingApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlite<MyContext>("Data Source=contacts.db");
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddInstantAPIs(options =>
+{
+    options.EnableSwagger = Fritz.InstantAPIs.EnableSwagger.DevelopmentOnly;
+    options.Swagger = config =>
+    {
+        config.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "My Working API",
+            Description = "An ASP.NET Core Web API"
+        });
+    };
+});
 
 var app = builder.Build();
 
@@ -19,7 +32,7 @@ app.MapInstantAPIs<MyContext>(options =>
 });
 Console.WriteLine($"Elapsed time to build InstantAPIs: {sw.Elapsed}");
 
-app.UseSwagger();
-app.UseSwaggerUI();
+//app.UseSwagger();
+//app.UseSwaggerUI();
 
 app.Run();
