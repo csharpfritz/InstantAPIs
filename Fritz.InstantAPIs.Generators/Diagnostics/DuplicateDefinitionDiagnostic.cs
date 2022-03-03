be6d7084812621977fad782f51fc413de587d05c
Jason Bock <jason.r.bock@outlook.com>
2022-03-03 07:45:28 -0600

    Creating a source generator (#19)
    * First NuGet Release (#16)
    
    * Added NuGet publish
    
    * Updated to only deploy when release is merged
    
    * Separating publish step
    
    * Added release to build process on PR
    
    * #12 FINALLY got the source generator to work
    
    * #12 YES!! The source generator is working!
    
    * #12 got the parsing methods working
    
    * #12 added more tests
    
    * #12 added support for key attribute
    
    * #12 added some comment notes on how to use the source generator in the web app
    
    * #12 removed some code for key attributes that's no longer used
    
    * #12 added a way to do configuration
    
    * #12 made progress on the new approach that has configuration. Tests need to be reworked, but it seems promising
    
    * #12 hey, it works in the WorkingApi project!
    
    * #12 all tests pass now
    
    * #12 got all the API verbs in now
    
    * #12 changed "db.Set<tableType>" to "db.{propertyName}"
    
    * #12 very minor code ordering change
    
    * Publishing release with new JSON APIs (#44)
    
    * Added video demo
    
    * adding JSON Mock API
    
    * adding instructions
    
    * Added HTTP Results calls to fix #7
    
    * Added configuration for JSON APIs
    
    * Tagging for a preview release
    
    * Added Release Notes file
    
    * Moved release notes to top level
    
    Co-authored-by: softchris <noringc@gmail.com>
    
    * #12 updated to IEndpointRouteBuilder
    
    * #12 got the APIs to return the "right" thing
    
    * #12 confirmed new changes work in API app
    
    * #12 NICE! Just updated config for SG, initial results are good, added a separate WorkingApi.Generators project to run the SG in its own project.
    
    * #12 updated configuration to take an Action<Builder>, makes it more similiar to the Reflection approach
    
    * #12 fixed an issue with ignoring Included value
    
    * #12 started working on logging, tests WILL break right now
    
    * #12 logging is in, all tests pass now.
    
    * #12 added an attribute approach to define which DbContext classes should be mapped
    
    * #12 got all tests for the SG approach in (for now)
    
    Co-authored-by: Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
    Co-authored-by: softchris <noringc@gmail.com>

---
+++ Fritz.InstantAPIs.Generators/Diagnostics/DuplicateDefinitionDiagnostic.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Fritz.InstantAPIs.Generators.Diagnostics;
+
+public class DuplicateDefinitionDiagnostic
+{
+	public static Diagnostic Create(SyntaxNode currentNode) =>
+		Diagnostic.Create(new DiagnosticDescriptor(
+			DuplicateDefinitionDiagnostic.Id, DuplicateDefinitionDiagnostic.Title,
+			DuplicateDefinitionDiagnostic.Message, DescriptorConstants.Usage, DiagnosticSeverity.Warning, true,
+			helpLinkUri: HelpUrlBuilder.Build(
+				DuplicateDefinitionDiagnostic.Id, DuplicateDefinitionDiagnostic.Title)),
+			currentNode.GetLocation());
+
+	public const string Id = "IA2";
+	public const string Message = "The given DbContext has already been defined.";
+	public const string Title = "Duplicate DbContext Definition";
+}
